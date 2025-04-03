2a48a3e6b2f0054e11abc6207dde84c1d596d25f
MarkSian <merksianipar@gmail.com>
2025-04-02 19:37:19 -0600

    Update scripts.js
    added Question Bank

---
+++ scripts/scripts.js
@@ -0,0 +1,54 @@
+// Question List
+const questionBank = {
+    pokemonTrivia: [
+        {
+            question: "Which Pokémon is known as the 'Pikachu Clone' of the Hoenn region?",
+            options: ["Plusle", "Pachirisu", "Togedemaru", "Morpeko"],
+            answer: 0
+        },
+        {
+            question: "Which Pokémon is the mascot of the Pokémon franchise?",
+            options: ["Charizard", "Pikachu", "Eevee", "Jigglypuff"],
+            answer: 1
+        },
+        {
+            question: "What is the National Dex number of Bulbasaur?",
+            options: ["1", "25", "151", "4"],
+            answer: 0
+        }
+    ],
+    pokemonMechanics: [
+        {
+            question: "Which move can hit a Pokémon using Fly?",
+            options: ["Thunder", "Earthquake", "Shadow Ball", "Hyper Beam"],
+            answer: 0
+        },
+        {
+            question: "Which of these abilities negates all weather effects?",
+            options: ["Cloud Nine", "Overcoat", "Magic Guard", "Ice Body"],
+            answer: 0
+        },
+        {
+            question: "Which item is needed to evolve Sneasel into Weavile?",
+            options: ["Razor Claw", "Razor Fang", "Dusk Stone", "Ice Stone"],
+            answer: 0
+        }
+    ],
+    pokemonHistory: [
+        {
+            question: "In which year was the first Pokémon game released?",
+            options: ["1996", "1998", "2000", "1994"],
+            answer: 0
+        },
+        {
+            question: "What was the first Pokémon ever designed?",
+            options: ["Pikachu", "Rhydon", "Bulbasaur", "Arceus"],
+            answer: 1
+        },
+        {
+            question: "Which region was introduced in Pokémon Black and White?",
+            options: ["Sinnoh", "Unova", "Galar", "Alola"],
+            answer: 1
+        }
+    ]
+};
